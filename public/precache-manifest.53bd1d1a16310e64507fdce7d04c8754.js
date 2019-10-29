e986b493eed0f33014551be962fcfda24f76dbbd
IAmOscar-Liu <karta0989006@gmail.com>
2019-10-29 17:36:23 +0800

    Initial commit

---
+++ public/precache-manifest.53bd1d1a16310e64507fdce7d04c8754.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89d4afb0831ab5a62b9c2a0519c8452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34d30440cb776a828f97",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1cbae346c6c29b811fd9",
+    "url": "/static/js/2.17b90086.chunk.js"
+  },
+  {
+    "revision": "34d30440cb776a828f97",
+    "url": "/static/js/main.99dfd64a.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Load env
+dotenv.config({ path: './config.env' });
+
+const app = express();
+app.use(cors());
+
+// Dev logging
+if (process.env.NODE_ENV === 'development') {
+    console.log('development mode');
+    app.use(morgan('dev'));
+  }
+
+app.get('/', (req, res) => {
+    res.send('This is the homepage');
+})
+
+app.get('/test', (req, res) => {
+  res.json({
+    "message" : "It works!"
+  })
+})
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
